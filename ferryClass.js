3e8025e25c0f3664c1c32916cbf86025daf6b845
christianraymond <ombenisn.raymond@gmail.com>
2017-11-13 11:37:31 +0200

    looping through cars

--- ferryClass.js
+++ ferryClass.js
@@ -1,38 +1,39 @@
-FerryClass = function(car, passengers){
-    this.car = car;
-    this.passengers = passengers;
-  }
+var CarClass = require('./carClass');
+FerryClass = function(car, passengers) {
+  this.car = car;
+  this.passengers = passengers;
+}
 
-  var carsInstance = [];
-    for(var x=1; x<= 20; x++){
-      var result = carsInstance.push(x)
-    }
-    if (carsInstance.length >= 20) {
-      console.log("Rejected!");
-    } else {
-      console.log("Accepted!");
-    }
+var carsInstance = [];
+for (var x = 1; x <= 20; x++) {
+  var result = carsInstance.push(x)
+}
+if (carsInstance.length >= 20) {
+  console.log("Rejected!");
+} else {
+  console.log("Accepted!");
+}
 var ferryInstances = new FerryClass(20, 40);
 console.log("There are " + ferryInstances.car + " cars and " + ferryInstances.passengers + " passengers in the Ferry!");
 
 
 var Dog = function(name) {
-    this.name = name;
+  this.name = name;
 
-    var barkCount = 0;
+  var barkCount = 0;
 
-    this.bark = function() {
-        barkCount++;
-        console.log(this.name + " bark");
-    };
+  this.bark = function() {
+    barkCount++;
+    console.log(this.name + " bark");
+  };
 
-    this.getBarkCount = function() {
-        console.log(this.name + " has barked " + barkCount + " times");
-    };
+  this.getBarkCount = function() {
+    console.log(this.name + " has barked " + barkCount + " times");
+  };
 };
 
 Dog.prototype.wagTail = function() {
-    console.log(this.name + " wagging tail");
+  console.log(this.name + " wagging tail");
 };
 
 var dog = new Dog("Dave");

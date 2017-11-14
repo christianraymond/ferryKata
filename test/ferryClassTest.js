9f5bde92d769211a5b6ae09e45b38b682c7cb314
christianraymond <ombenisn.raymond@gmail.com>
2017-11-14 10:57:29 +0200

    push after pull from another machine

--- test/ferryClassTest.js
+++ test/ferryClassTest.js
@@ -8,17 +8,19 @@ describe('The Ferry class constructor', function() {
   it('should specify the number of cars allowed on the ferry and also how many people are allowed on the ferry.', function() {
     var newFerry = new ferry(20, 40);
     console.log(newFerry);
-    assert.deepEqual(newFerry, { car: 20, passengers: 40 }
-)
+    assert.deepEqual(newFerry, {
+      car: 20,
+      passengers: 40
+    })
   })
-   it('should allow Cars into the Ferry using the board method.', function() {
-     
-   })
-  //  it("should check If the ferry is not full yet then the board method returns ‘accepted’.", function() {})
-  //  it("should check If there are enough people or cars on the Ferry already, the board method should return ‘rejected’.", function() {})
-  //  it("The Ferry class instance should keep track of the number of people and cars on the Ferry using the people_count and car_count properties.", function() {})
-  //  it('should know how many cars of a certain color are on the Ferry', function(){});
-  //  it('should update the people and car counts correctly when a car leaves the Ferry', function(){});
-  //  it("should give a car 50% discount after three trips on the same ferry. The board method should return ‘half price!’", function(){})
-  //  it("should give a free trip after 7 trips on any Ferry. The board method should return ‘you go free!’", function(){});
+  it('should allow Cars into the Ferry using the board method.', function() {
+
+  })
+  it("should check If the ferry is not full yet then the board method returns ‘accepted’.", function() {})
+  it("should check If there are enough people or cars on the Ferry already, the board method should return ‘rejected’.", function() {})
+  it("The Ferry class instance should keep track of the number of people and cars on the Ferry using the people_count and car_count properties.", function() {})
+  it('should know how many cars of a certain color are on the Ferry', function() {});
+  it('should update the people and car counts correctly when a car leaves the Ferry', function() {});
+  it("should give a car 50% discount after three trips on the same ferry. The board method should return ‘half price!’", function() {})
+  it("should give a free trip after 7 trips on any Ferry. The board method should return ‘you go free!’", function() {});
 })

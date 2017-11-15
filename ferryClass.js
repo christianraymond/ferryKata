19e66be76917e777283a056e0c7f4c1cfb2242e2
christianraymond <ombenisn.raymond@gmail.com>
2017-11-15 15:58:30 +0200

    use the board method to add cars to the ferry

--- ferryClass.js
+++ ferryClass.js
@@ -1,20 +1,33 @@
 module.exports = function() {
-  var FerryClass = function(car, passengers) {
-    this.car = car;
+  var FerryClass = function(cars, passengers) {
+    this.cars = cars;
     this.passengers = passengers;
   }
 
-  this.onBoard = function(){
-  var store = [];
-  var countCars = Math.floor(Math.random() * 30) + 1;
-  for (var x = 0; x <= countCars; x++) {
-    var result = store.push(x)
+  board = function() {
+    //add cars in the ferry using board method
+    var carsData = {
+      Model: 'Chevrolet',
+      Reg: 'CA 123',
+      Color: 'Gray',
+      AmountOfCars: Math.floor(Math.random() * 100) + 1 + ' Cars.'
+    }
+    for (var key in carsData) {
+      if (carsData.hasOwnProperty(key)) {
+        console.log(key + ' => ' + carsData[key])
+      }
+    }
+    //check if the amount of cars ecceed certain amoun and display REJECTED&&Acepted.
+    var store = [];
+    var AmountOfCars = Math.floor(Math.random() * 100) + 1;
+    for (var x = 0; x <= AmountOfCars; x++) {
+      var result = store.push(x)
+    }
+    if (result >= 21) {
+      console.log("REJECTED!")
+    } else {
+      console.log("Acepted!")
+    }
   }
-  if (result >= 21) {
-    console.log("REJECTED!")
-  } else {
-    console.log("Acepted!")
-  }
-}
   return FerryClass;
 }
